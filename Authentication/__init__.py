b57090cf56eeef9f1f70132cd5d5296add9fc691
wangrui <wangrui@mindflow.com.cn>
2022-11-27 13:51:36 +0800

    2022/11/27：接口鉴权

---
+++ Authentication/__init__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# cython: language_level=3
+# -*- coding:utf-8 -*-
+# @filename : script.py
+# @package :
+# @Time : 2022/11/24 16:06
+# @Author : 山而
+# @desc :
+import os
+import json
+import glob
+
+file_dir = "json"
+input_dir = f"input/{file_dir}"
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
